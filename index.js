efbd2e2e13c4e94f711a1f73541f781a7b726706
Evan Hong <evangelurtz@gmail.com>
2020-01-06 16:41:41 +0900

    Add create and get functions

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.log('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Evan',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+
+
+    const courses = await Course
+        //.find({ author: 'Evan', isPublished: true })
+        .find({ author: /.*Evan.*/})
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1})
+        .count();
+    console.log(courses);
+}
+
+getCourses();
